1448fb2de6c0369db6b21bc4faa31e4180065478
Khaldoun1967 <144149465+Khaldoun1967@users.noreply.github.com>
2023-09-16 15:05:20 +0300

    Add files via upload

---
+++ Serial-Terminal - simulation/Serial-Terminal/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Serial_Terminal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(192, 37);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(431, 339);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(358, 147);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(868, 398);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Device Rating Information";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(9, 169);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(164, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "CPU Firmware version";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(9, 15);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(164, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "QPIRI Device Rating Information";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(9, 209);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(164, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "QMN";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(630, 37);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(418, 339);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(9, 93);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(164, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Device general status parameters";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(9, 44);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(164, 37);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "QPIGS general status parameters";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(227, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(681, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = " (202.3 48.9 202.3 48.9 0809 0726 020 425 28.20 015 095 0038 06.1 142.2 00.00 000" +
+    "00 00010110 00 00 00839 011 0 00 0000}\\r";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(935, 9);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "Send";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(210, 398);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "sent file";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(303, 396);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "Next";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(418, 398);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(362, 23);
+            this.progressBar1.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(579, 431);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "0";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(56, 266);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 17);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "Allow respond";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 456);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Serial-Terminal - simulation/Serial-Terminal/Form1.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+
+namespace Serial_Terminal
+{
+    public partial class Form1 : Form
+    {
+        SerialPort Com;
+        byte[] SerBuf = new byte[10];
+
+        string textFile =  "C:\\Users\\Admin\\Desktop\\solar\\Voltronic protocol\\sent.txt";
+        string[] lines;
+        int lines_no;
+        int current_line_no;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            listBox1.Items.Clear();
+
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            string[] ports = SerialPort.GetPortNames();
+            if (ports.Length == 0)
+            {
+                MessageBox.Show("No Communecation port was detected!!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //Thread.CurrentThread.Abort();
+                this.Close();
+            }
+
+            foreach (string port in ports)
+            {
+                listBox1.Items.Add(port);
+            }
+
+        }
+
+        void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string portNo = (string)listBox1.GetItemText(listBox1.SelectedItem);
+            if (portNo == "")
+            {
+                MessageBox.Show("No Communecation port was Selected!!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(-1);
+            }
+
+            listBox1.Visible = false;
+
+            Com = new SerialPort(portNo);
+
+            Com.Open();
+
+            Com.BaudRate = 2400; //115200;
+            Com.DataBits = 8;
+            Com.DataReceived += Com_DataReceived;
+
+            Com.Disposed += Com_Disposed;
+        }
+
+        void Com_Disposed(object sender, EventArgs e)
+        {
+            MessageBox.Show("Serial Port has been terminated!!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Environment.Exit(-1);
+        }
+
+        //bool Query_being_recived = false;
+        void Com_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int size = Com.BytesToRead;
+            byte[] X = new byte[size];
+            Com.Read(X, 0, size);
+            String STR = "";
+            
+            string STR2 = System.Text.Encoding.ASCII.GetString(X);
+            STR2 = STR2.Replace(' ', '\n');
+          //  string[] STRs = STR2.Split(' ');
+            CrossThreadShow2(STR2, true);
+
+            for (int i = 0; i < size; i++)
+            {
+                STR += X[i].ToString("X")+" ";
+                if (X[i] == 0xd) STR += Environment.NewLine;
+            }
+            CrossThreadShow(STR, true);
+
+            Send_Qeury(textBox1.Text);
+            //send_string(textBox1.Text);
+        }
+
+       void Scroll_to_careat1()
+        {
+            // set the current caret position to the end
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            // scroll it automatically
+            richTextBox1.ScrollToCaret();
+        }
+
+       void Scroll_to_careat2()
+       {
+           // set the current caret position to the end
+           richTextBox2.SelectionStart = richTextBox2.Text.Length;
+           // scroll it automatically
+           richTextBox2.ScrollToCaret();
+       }
+        void CrossThreadShow(String str, bool Add)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() =>
+                { 
+                    if (Add) richTextBox1.Text += str;
+                    else richTextBox1.Text = str;
+                    richTextBox1.SelectionStart
+                        = richTextBox1.Text.Length;
+                    Scroll_to_careat1();
+                }));
+            }
+            else
+            {
+                if (Add) richTextBox1.Text += str;
+                else richTextBox1.Text = str;
+                richTextBox1.SelectionStart
+                    = richTextBox1.Text.Length;
+                Scroll_to_careat1();
+            }
+        }
+
+
+        void CrossThreadShow2(String str, bool Add)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    if (Add) richTextBox2.Text += str;
+                    else richTextBox2.Text = str;
+                    richTextBox2.SelectionStart
+                        = richTextBox2.Text.Length;
+                    Scroll_to_careat2();
+                }));
+            }
+            else
+            {
+                if (Add) richTextBox2.Text += str;
+                else richTextBox2.Text = str;
+                richTextBox2.SelectionStart
+                    = richTextBox2.Text.Length;
+                Scroll_to_careat2();
+            }
+        }
+        
+        void Send_Qeury(string str)
+        {
+            string s = str + "\r";   //\r (0x0d) End of input character
+           
+            char[] characters = s.ToCharArray();
+            Byte[] encodedBytes = System.Text.Encoding.ASCII.GetBytes(s);
+            if(checkBox1.Checked == true)Com.Write(encodedBytes, 0, encodedBytes.Length);
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //QPIRI<cr>: Device Rating Information inquiry
+            Send_Qeury("QPIRI");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //QVFW2<cr> :Another CPU Firmware version inquiry
+            Send_Qeury("QVFW2");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            /*
+             Device Rating Information inquiry
+            51 50 49 52 49 F8 54 D
+            QPIRI?T
+            (220.0 18.1 220.0 50.0 18.1 4000 4000 24.0 23.0 21.0 28.2 27.0 0 030 030 0 1 2 1 01 0 0 27.0 0 1?
+             */
+            SerBuf[0] = 0x51;
+            SerBuf[1] = 0x50;
+            SerBuf[2] = 0x49;
+            SerBuf[3] = 0x52;
+            SerBuf[4] = 0x49;
+            SerBuf[5] = 0xf8;
+            SerBuf[6] = 0x54;
+            SerBuf[7] = 0xd;
+
+            Com.Write(SerBuf, 0, 8);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            /*
+             51 4D 4E BB 64 D
+            QMN?d
+            (VMIII-4000??
+             */
+            SerBuf[0] = 0x51;
+            SerBuf[1] = 0x4d;
+            SerBuf[2] = 0x4e;
+            SerBuf[3] = 0xbb;
+            SerBuf[4] = 0x64;
+            SerBuf[5] = 0xd;         
+            Com.Write(SerBuf, 0, 6);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Send_Qeury("QPIGS");//Device general status parameters inquiry
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            /*
+             Device general status parameters inquiry
+             51 50 49 47 53 B7 A9 D
+             QPIGS??
+             (202.3 48.9 202.3 48.9 0809 0726 020 425 28.20 015 095 0038 06.1 142.2 00.00 00000 00010110 00 00 00839 011 0 00 0000}?
+             */
+            SerBuf[0] = 0x51;
+            SerBuf[1] = 0x50;
+            SerBuf[2] = 0x49;
+            SerBuf[3] = 0x47;
+            SerBuf[4] = 0x53;
+            SerBuf[5] = 0xb7;
+            SerBuf[6] = 0xa9;
+            SerBuf[7] = 0x0d;
+
+            Com.Write(SerBuf, 0, 8);
+        }
+
+      
+
+        void send_line(string str)
+        {
+            string STR = str;
+            string[] ARRAY = STR.Split(' ');
+            byte[] ary = new byte[ARRAY.Length];
+            for (int i = 0; i < ARRAY.Length; i++)
+            {
+                ary[i] = Convert.ToByte(ARRAY[i], 16);
+
+            }
+            Com.Write(ary, 0, ary.Length);
+
+            string STR2 = System.Text.Encoding.ASCII.GetString(ary);
+
+            CrossThreadShow2(Environment.NewLine + STR + Environment.NewLine + STR2, true);
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string STR = textBox1.Text;
+            send_line(STR);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            // Read a text file line by line.
+            lines = File.ReadAllLines(textFile);
+            lines_no = lines.Length;
+            current_line_no = 0;
+
+             
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string str = lines[current_line_no];
+            str = str.Substring(0, str.Length-1);
+            send_line(str);
+            current_line_no++;
+            progressBar1.Value = (int)((100*current_line_no) / lines_no);
+            label1.Text = progressBar1.Value.ToString();
+        }
+
+    }
+}
+/*
+QPI<cr>: Device Protocol ID Inquiry
+51 50 49 B6 AC D
+QPI??
+(PI30?
+ 
+51 47 4D 4E 49 29 D
+QGMNI)
+(055??
+ 
+51 4D 4E BB 64 D
+QMN?d
+(VMIII-4000??
+
+Device Rating Information inquiry
+51 50 49 52 49 F8 54 D
+QPIRI?T
+(220.0 18.1 220.0 50.0 18.1 4000 4000 24.0 23.0 2
+
+51 4D 4E BB 64 D
+QMN?d
+1.0 28.2 27.0 0 030 030 0 1 2 1 01 0 0 27.0 0 1?
+
+The device ID inquiry
+51 53 49 44 BB 5 D
+QSID?
+(1496322206105690005535~K
+
+51 44 4F 50 85 E5 D
+QDOP??
+(1 3 2 0 00.0 21.0 015 40 010 030 000 000 00 23k?
+ 
+Main CPU Firmware version inquiry
+51 56 46 57 62 99 D
+QVFWb?
+(VERFW:00056.02d
+ 
+Device general status parameters inquiry
+51 50 49 47 53 B7 A9 D
+QPIGS??
+(202.3 48.9 202.3 48.9 0809 0726 020 425 28.20 015 095 0038 06.1 142.2 00.00 00000 00010110 00 00 00839 011 0 00 0000}?
+
+51 42 45 51 49 2E A9 D
+QBEQI.?
+(0 060 030 030 030 29.20 000 120 0 0000VX
+
+Device Rating Information inquiry
+51 50 49 52 49 F8 54 D
+QPIRI?T
+(220.0 18.1 220.0 50.0 18.1 4000 4000 24.0 23.0 21.0 28.2 27.0 0 030 030 0 1 2 1 01 0 0 27.0 0 1?
+ 
+51 46 4C 41 47 98 74 D
+QFLAG?t
+(EbkuvDadjxyz\
+
+The default setting value information
+51 44 49 71 1B D
+QDIq
+(230.0 50.0 0030 21.0 27.0 28.2 23.0 60 0 0 2 0 0 0 0 0 1 1 1 0 1 0 27.0 0 1)F
+
+Device Warning Status inquiry
+51 50 49 57 53 B4 DA D
+QPIWS??
+(000000000000000001000000000000000000??
+
+ 
+*/
